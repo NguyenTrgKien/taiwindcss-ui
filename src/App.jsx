35628eda4f2421c364093fa8dbd0da29fe59003c
TRUNGKIEN <nguyentrungkien040921@gmail.com>
2024-11-23 14:15:28 +0700

    update source code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ListCard from "./component/ListCard";
+
+function App() {
+  return (
+    <ListCard/>
+  )
+}
+
+export default App
---
+++ src/component/ListCard/index.jsx
@@ -0,0 +1,44 @@
+function ListCard() {
+    return (  
+        <div className="p-5 grid grid-cols-3 bg-black gap-5">
+            <div className="w-full h-auto text-center rounded-[8px] p-5 bg-cyan-700">
+                <img
+                    src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg"
+                    className="w-full h-[300px] rounded-[8px]"
+                />
+                <h2 className="mt-10px text-[40px] text-[red] font-bold">
+                    Image Card
+                </h2>
+                <p className="text-white mt-3">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis necessitatibus recusandae minus pariatur veritatis iste? Molestias aspernatur adipisci consequatur reiciendis a pariatur sed nihil? Esse excepturi perferendis reprehenderit culpa!
+                </p>
+            </div>
+            <div className="w-full h-auto text-center rounded-[8px] p-5 bg-cyan-700">
+                <img
+                    src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg"
+                    className="w-full h-[300px] rounded-[8px]"
+                />
+                <h2 className="mt-10px text-[40px] text-[red] font-bold">
+                    Image Card
+                </h2>
+                <p className="text-white mt-3">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis necessitatibus recusandae minus pariatur veritatis iste? Molestias aspernatur adipisci consequatur reiciendis a pariatur sed nihil? Esse excepturi perferendis reprehenderit culpa!
+                </p>
+            </div>
+            <div className="w-full h-auto text-center rounded-[8px] p-5 bg-cyan-700">
+                <img
+                    src="https://static.vecteezy.com/system/resources/thumbnails/024/669/489/small_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg"
+                    className="w-full h-[300px] rounded-[8px]"
+                />
+                <h2 className="mt-10px text-[40px] text-[red] font-bold">
+                    Image Card
+                </h2>
+                <p className="text-white mt-3">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus reiciendis necessitatibus recusandae minus pariatur veritatis iste? Molestias aspernatur adipisci consequatur reiciendis a pariatur sed nihil? Esse excepturi perferendis reprehenderit culpa!
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default ListCard;
